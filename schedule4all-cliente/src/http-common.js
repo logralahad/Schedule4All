e23fbaa6b48d8ce5b141cd3f5f1bab12f3588414
logralahad <logralahad@gmail.com>
2021-11-30 21:36:12 -0600

    vieja version

--- schedule4all-cliente/src/http-common.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-export default axios.create({
-    baseURL: "http://localhost:9595/schedule4all",
-    headers: {
-        "Content-type": "application/json"
-    }
-});
